7471f37256b394212216bbf42133dac5844c3e63
Abel AR <abel_john_14@hotmail.com>
2021-11-23 22:14:58 -0500

    Fix: Add colors in component UI and create environment variables

---
+++ src/components/UI/colors.tsx
@@ -0,0 +1,12 @@
+export const colors = {
+    primary2: "#EBA59E",
+    primary3: "#E27D73",
+    primary4: "#DA584B",
+    secondary: "#70B252",
+    tertiary: "#E5B454",
+    neutral: "#FFFFFF",
+    neutral2: "#94979A",
+    neutral3: "#393D41",
+    neutral4: "#2C2F33",
+    neutral5: "#222528",
+}
